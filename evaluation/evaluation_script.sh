e7010656794a43f3f126906884ab67e39a95cdff
silee <63944028+childult-programmer@users.noreply.github.com>
2024-02-19 21:40:37 +0900

    Delete evaluation directory Directory

--- evaluation/evaluation_script.sh
+++
@@ -1,8 +0,0 @@
-OMP_NUM_THREADS=4 python evaluation_script.py \
-    --annFile ./KAIST_annotation.json \
-    --rstFile state_of_arts/INSANet_result.txt \
-              state_of_arts/CFR_result.txt \
-              state_of_arts/GAFF_result.txt \
-              state_of_arts/MLPD_result.txt \
-              state_of_arts/MBNet_result.txt
-    --evalFig KAIST_BENCHMARK.jpg
