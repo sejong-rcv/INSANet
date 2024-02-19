ae24e88cc156e8a082775fd054ee30fa4653dd66
silee <63944028+childult-programmer@users.noreply.github.com>
2024-02-19 21:49:06 +0900

    Update inference.sh

--- src/script/inference.sh
+++ src/script/inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
 
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python inference.py \
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python inference.py \
     --model-path '../pretrained/pretrained.pth.tar' \
     --result-dir './results/pretrained'
