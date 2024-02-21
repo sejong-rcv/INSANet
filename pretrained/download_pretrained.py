934ececd59cc025620ce174233077675a3436bf5
silee <63944028+childult-programmer@users.noreply.github.com>
2024-02-21 13:36:11 +0900

    Update ggid

--- pretrained/download_pretrained.py
+++ pretrained/download_pretrained.py
@@ -4,9 +4,9 @@ import gdown
 
 print('Download checkpoint... ')
 
-ggid = "1iP1XvRu0mvcGLSidY9ML0tVRUJrdPcRm"
+ggid = "1yL6H0x8pTaOqmZ6U05kFiTHfx_MO6YdW"
 ggfile = "pretrained.pth.tar"
 
 gdown.download(f'https://drive.google.com/uc?id={ggid}', output=ggfile)
 
-print('Done. Please try "python src/eval.py --model pretrained/pretrained.pth.tar"')
+print('Done. Please try "python src/script/inference.sh"')
