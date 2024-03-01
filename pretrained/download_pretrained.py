8e494ae70dc5fa51db1788c50f7c74e01d958c1f
silee <63944028+childult-programmer@users.noreply.github.com>
2024-03-01 19:01:19 +0900

    Update download_pretrained.py

--- pretrained/download_pretrained.py
+++ pretrained/download_pretrained.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
 import gdown
 
 print('Download checkpoint... ')
 
-ggid = "1yL6H0x8pTaOqmZ6U05kFiTHfx_MO6YdW"
+ggid = "1C56Jq1K2TuXFAp9f5UDkSF7Y-FucAG0L"
 ggfile = "pretrained.pth.tar"
 
 gdown.download(f'https://drive.google.com/uc?id={ggid}', output=ggfile)
