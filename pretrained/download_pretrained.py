f2550062a5bdac742447e084117fb19c0320691e
silee <63944028+childult-programmer@users.noreply.github.com>
2024-03-04 11:28:36 +0900

    Update download_pretrained.py

--- pretrained/download_pretrained.py
+++ pretrained/download_pretrained.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import gdown
+import shutil
 
 print('Download checkpoint... ')
 
@@ -7,5 +8,10 @@ ggid = "1C56Jq1K2TuXFAp9f5UDkSF7Y-FucAG0L"
 ggfile = "pretrained.pth.tar"
 
 gdown.download(f'https://drive.google.com/uc?id={ggid}', output=ggfile)
+
+try:
+    shutil.move(ggfile, './pretrained/pretrained.pth.tar')
+except FileNotFoundError:
+    pass
 
 print('Done. Please try "python src/script/inference.sh"')
